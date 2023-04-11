bdd08f2aa3591b875d89c5c150816a783b3a19d4
Christian Casey <christian.b.casey@gmail.com>
2023-04-11 15:25:14 -0700

    Create factory for User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :user, aliases: %i[author friend] do
+  factory :user, aliases: %i[author friend sender receiver] do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
   end
