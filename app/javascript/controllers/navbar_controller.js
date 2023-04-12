baf0661dab65ec322e6e6dfa93a6ba7d165d346d
Christian Casey <christian.b.casey@gmail.com>
2023-04-11 20:40:36 -0700

    Display friend requests in navbar

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  static targets = [ "burger", "dropdown" ];
+  static classes = [ "change" ];
+
+  toggleBurger() {
+    this.burgerTargets.forEach(target => {
+      target.classList.toggle(this.changeClass);
+    });
+  }
+
+  toggleDropdown() {
+    this.dropdownTarget.classList.toggle(this.changeClass);
+  }
+}
