5a8866215ad1b3bcddbccb71bf085eadfdb63098
Christian Casey <christian.b.casey@gmail.com>
2023-04-15 18:58:03 -0700

    Order posts on user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @friendship = Friendship.locate_friendship(current_user.id, @user.id) || Friendship.new
+    @posts = @user.posts.ordered
   end
 end
