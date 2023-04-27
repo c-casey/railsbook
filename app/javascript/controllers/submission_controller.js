84d80660c07a104f80377131dee399978c997750
Christian Casey <christian.b.casey@gmail.com>
2023-04-27 13:07:31 -0700

    Disable post/comment submission on empty field

--- app/javascript/controllers/submission_controller.js
+++ app/javascript/controllers/submission_controller.js
@@ -2,7 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="submission"
 export default class extends Controller {
+  static targets = [ "button", "textarea" ]
+  static classes = [ "disabled" ]
+
   clear() {
     this.element.reset()
+  }
+
+  toggleButton() {
+    if (this.textareaTarget.value == "") {
+      this.disableButton()
+    } else {
+      this.enableButton()
+    }
+  }
+
+  enableButton() {
+    this.buttonTarget.disabled = false
+  }
+
+  disableButton() {
+    this.buttonTarget.disabled = true
   }
 }
