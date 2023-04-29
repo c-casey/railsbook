7c151b0f0dcf636591f449fa88f5a0c5ee2781e5
Christian Casey <christian.b.casey@gmail.com>
2023-04-29 16:59:06 -0700

    Automate logins for Capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Allow automated logins for Capybara
+  config.include Warden::Test::Helpers
 end
