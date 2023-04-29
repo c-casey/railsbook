3695d4f187156c5c39ce2bb75132038fea3295b4
Christian Casey <christian.b.casey@gmail.com>
2023-04-29 16:59:39 -0700

    Test Like associations

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#likeable" do
+    context "when likeable is a Post" do
+      let(:author) { FactoryBot.create(:user) }
+      let(:likeable) { FactoryBot.create(:post) }
+      subject(:like) { Like.new(author: author, likeable: likeable) }
+
+      it "returns the Post" do
+        result = like.likeable
+        expect(result).to be_a(Post)
+      end
+    end
+
+    context "when likeable is a Comment" do
+      let(:author) { FactoryBot.create(:user) }
+      let(:likeable) { FactoryBot.create(:comment) }
+      subject(:like) { Like.new(author: author, likeable: likeable) }
+
+      it "returns the Comment" do
+        result = like.likeable
+        expect(result).to be_a(Comment)
+      end
+    end
+  end
 end
