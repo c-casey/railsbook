ee681be61957c925f79872e84ae68425995d41cb
Christian Casey <christian.b.casey@gmail.com>
2023-05-01 17:39:29 -0700

    Fetch user avatars from Gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
+require 'digest'
+
 module UsersHelper
+  def avatar_url(user:, size: 64)
+    email = user.email.strip.downcase
+    md5 = Digest::MD5.hexdigest(email)
+    "https://www.gravatar.com/avatar/#{md5}?d=mp&s=#{size}"
+  end
 end
