7314e39b4b1376bdbc236106aafd769d1b9e3164
Christian Casey <christian.b.casey@gmail.com>
2023-06-26 10:10:58 -0700

    Complete REST functionality for Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,11 @@ class CommentsController < ApplicationController
     @comments = @post.comments.includes(:author)
   end
 
+  def show
+    @comment = Comment.find(params[:id])
+    @post = @comment.parent
+  end
+
   def new
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build
@@ -21,6 +26,34 @@ class CommentsController < ApplicationController
       end
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      respond_to do |format|
+        format.html { redirect_to @comment, status: :see_other }
+        format.turbo_stream
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.parent
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to @post }
+      format.turbo_stream
     end
   end
 
